9722e5911e749ce7e4d15bd2ce07238d2ed3b2b8
Stefan Berthold <stefan.berthold@gmx.net>
2017-09-14 22:19:39 +0200

    added Map.

--- map.go
+++ map.go
@@ -4,8 +4,42 @@ import (
 	"reflect"
 )
 
+var (
+	AssignMap func(sun, fun interface{})
+)
+
+func init() {
+	MakeAssignment(&AssignMap, Map)
+}
+
 func MakeMap(fun interface{}) {
-	funT := reflect.TypeOf(fun).Elem()
+	reflect.ValueOf(fun).Elem().Set(makeMap(reflect.TypeOf(fun).Elem()))
+}
+
+func Map(fun interface{}) interface{} {
+	funT := reflect.TypeOf(fun)
+	runT := reflect.FuncOf(
+		[]reflect.Type{
+			reflect.FuncOf(
+				[]reflect.Type{funT.In(0)},
+				[]reflect.Type{funT.Out(0)},
+				false),
+			reflect.SliceOf(funT.In(0))},
+		[]reflect.Type{reflect.SliceOf(funT.Out(0))},
+		false)
+	runV := makeMap(runT)
+
+	return reflect.MakeFunc(
+		reflect.FuncOf(
+			[]reflect.Type{reflect.SliceOf(funT.In(0))},
+			[]reflect.Type{reflect.SliceOf(funT.Out(0))},
+			false),
+		func(args []reflect.Value) []reflect.Value {
+			return runV.Call(append([]reflect.Value{reflect.ValueOf(fun)}, args...))
+		}).Interface()
+}
+
+func makeMap(funT reflect.Type) reflect.Value {
 	gunT := funT.In(0)
 	sunT := reflect.FuncOf(
 		[]reflect.Type{
@@ -17,17 +51,15 @@ func MakeMap(fun interface{}) {
 		[]reflect.Type{reflect.SliceOf(gunT.Out(0))},
 		false)
 
-	reflect.ValueOf(fun).Elem().
-		Set(
-			reflect.MakeFunc(
-				sunT,
-				func(args []reflect.Value) (results []reflect.Value) {
-					res := reflect.MakeSlice(reflect.SliceOf(gunT.Out(0)), 0, 0)
-					for i := 0; i < args[1].Len(); i++ {
-						res = reflect.Append(
-							res,
-							args[0].Call([]reflect.Value{args[1].Index(i)})[0])
-					}
-					return []reflect.Value{res}
-				}))
+	return reflect.MakeFunc(
+		sunT,
+		func(args []reflect.Value) []reflect.Value {
+			res := reflect.MakeSlice(reflect.SliceOf(gunT.Out(0)), 0, 0)
+			for i := 0; i < args[1].Len(); i++ {
+				res = reflect.Append(
+					res,
+					args[0].Call([]reflect.Value{args[1].Index(i)})[0])
+			}
+			return []reflect.Value{res}
+		})
 }
--- map_test.go
+++ map_test.go
@@ -17,6 +17,23 @@ func TestMakeMap(t *testing.T) {
 	for i, v := range expected {
 		if v != res[i] {
 			t.Errorf("expected %#v, got %#v", expected, res)
+			break
+		}
+	}
+}
+
+func TestAssignMap(t *testing.T) {
+	var mapper func([]int) []int
+	AssignMap(&mapper, func(arg int) int { return arg + 4 })
+
+	var expected = []int{5, 6, 7, 8, 9}
+
+	res := mapper([]int{1, 2, 3, 4, 5})
+
+	for i, v := range expected {
+		if v != res[i] {
+			t.Errorf("expected %#v, got %#v", expected, res)
+			break
 		}
 	}
 }

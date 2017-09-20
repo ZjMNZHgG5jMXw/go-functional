873f7e389afd359b01d935977c9457b7e0005c5b
Stefan Berthold <stefan.berthold@gmx.net>
2017-09-20 21:31:43 +0200

    removed makeMap().

--- map.go
+++ map.go
@@ -13,7 +13,30 @@ func init() {
 }
 
 func MakeMap(fun interface{}) {
-	reflect.ValueOf(fun).Elem().Set(makeMap(reflect.TypeOf(fun).Elem()))
+	funT := reflect.TypeOf(fun).Elem()
+	gunT := funT.In(0)
+	sunT := reflect.FuncOf(
+		[]reflect.Type{
+			reflect.FuncOf(
+				[]reflect.Type{gunT.In(0)},
+				[]reflect.Type{gunT.Out(0)},
+				false),
+			reflect.SliceOf(gunT.In(0))},
+		[]reflect.Type{reflect.SliceOf(gunT.Out(0))},
+		false)
+
+	sunV := reflect.MakeFunc(
+		sunT,
+		func(args []reflect.Value) []reflect.Value {
+			res := reflect.MakeSlice(reflect.SliceOf(gunT.Out(0)), 0, 0)
+			for i := 0; i < args[1].Len(); i++ {
+				res = reflect.Append(
+					res,
+					args[0].Call([]reflect.Value{args[1].Index(i)})[0])
+			}
+			return []reflect.Value{res}
+		})
+	reflect.ValueOf(fun).Elem().Set(sunV)
 }
 
 func Map(fun interface{}) interface{} {
@@ -27,7 +50,8 @@ func Map(fun interface{}) interface{} {
 			reflect.SliceOf(funT.In(0))},
 		[]reflect.Type{reflect.SliceOf(funT.Out(0))},
 		false)
-	runV := makeMap(runT)
+	runV := reflect.New(runT)
+	MakeMap(runV.Interface())
 
 	return reflect.MakeFunc(
 		reflect.FuncOf(
@@ -35,31 +59,7 @@ func Map(fun interface{}) interface{} {
 			[]reflect.Type{reflect.SliceOf(funT.Out(0))},
 			false),
 		func(args []reflect.Value) []reflect.Value {
-			return runV.Call(append([]reflect.Value{reflect.ValueOf(fun)}, args...))
+			return runV.Elem().
+				Call(append([]reflect.Value{reflect.ValueOf(fun)}, args...))
 		}).Interface()
-}
-
-func makeMap(funT reflect.Type) reflect.Value {
-	gunT := funT.In(0)
-	sunT := reflect.FuncOf(
-		[]reflect.Type{
-			reflect.FuncOf(
-				[]reflect.Type{gunT.In(0)},
-				[]reflect.Type{gunT.Out(0)},
-				false),
-			reflect.SliceOf(gunT.In(0))},
-		[]reflect.Type{reflect.SliceOf(gunT.Out(0))},
-		false)
-
-	return reflect.MakeFunc(
-		sunT,
-		func(args []reflect.Value) []reflect.Value {
-			res := reflect.MakeSlice(reflect.SliceOf(gunT.Out(0)), 0, 0)
-			for i := 0; i < args[1].Len(); i++ {
-				res = reflect.Append(
-					res,
-					args[0].Call([]reflect.Value{args[1].Index(i)})[0])
-			}
-			return []reflect.Value{res}
-		})
 }
